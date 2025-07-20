1df4d4c5c1e77d49fa8bc02e9cd1bf0a7919ebe8
dbkegley <david@kegley.me>
2025-07-19 23:05:10 -0400

    Install extra packages

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     # Nixpkgs
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "nixpkgs/nixpkgs-unstable";
 
     # Home Manager
     home-manager = {
@@ -12,13 +13,19 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }: let
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }: let
     me = "david";
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
+    pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
   in {
     homeConfigurations = {
       "${me}" = home-manager.lib.homeManagerConfiguration {
+
+        # TODO: figure out how to pass pkgs-unstable into home.nix to install helix latest
+        # or just use unstable as the default instead?
+
+        # inherit pkgs pkgs-unstable;
         inherit pkgs;
         modules = [
           ./home-manager/home.nix
--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,11 +1,24 @@
 # nix/home-manager/home.nix
 
-{ config, pkgs, ... }:
+{ config, pkgs, pkgs-unstable, ... }:
 
 let
   me = "david";
   email = "david@kegley.me";
   github = "dbkegley";
+  packages = with pkgs; [
+    git
+    curl
+    font-awesome_5
+    nerdfonts
+    uv
+    go
+    helix
+    zellij
+  ];
+  # packages-unstable = with pkgs-unstable; [
+  #   helix
+  # ];
 in {
   nix = {
     package = pkgs.nix;
@@ -14,23 +27,23 @@ in {
   home.username = "${me}";
   home.homeDirectory = "/home/${me}";
   home.stateVersion = "24.11";
-  home.packages = with pkgs; [
-    git
-    curl
-    font-awesome_5
-    nerdfonts
-    helix
-  ];
+  # home.packages = packages ++ packages-unstable;
+  home.packages = packages;
+  home.file = {
+  #   ".zshrc".source = config.lib.file.mkOutOfStoreSymlink ./dotfiles/.zshrc;
+    ".config/helix/config.toml".source = ../dotfiles/helix/config.toml;
+    ".config/helix/languages.toml".source = ../dotfiles/helix/languages.toml;
+  };
   programs = {
     home-manager.enable = true;
     fzf.enable = true;
     jq.enable = true;
-    direnv = {
-      enable = true;
-      nix-direnv = {
-        enable = true;
-      };
-    };
+    # direnv = {
+    #   enable = true;
+    #   nix-direnv = {
+    #     enable = true;
+    #   };
+    # };
     git = {
       enable = true;
       userName = "${github}";
@@ -42,6 +55,11 @@ in {
     };
     zsh = {
       enable = true;
+      oh-my-zsh = {
+        enable = true;
+        theme = "robbyrussell";
+        plugins = ["git"];
+      };
       autosuggestion.enable = true;
       # completions.enable = true;
       syntaxHighlighting.enable = true;
@@ -55,7 +73,7 @@ in {
         share = false;
         size = 10000;
       };
-      # profileExtra = "if [ -e $HOME/.nix-profile/etc/profile.d/nix.sh ]; then . $HOME/.nix-profile/etc/profile.d/nix.sh; fi";
+      profileExtra = "if [ -e $HOME/.nix-profile/etc/profile.d/nix.sh ]; then . $HOME/.nix-profile/etc/profile.d/nix.sh; fi";
     };
   };
 
