e8ec85b4a1b5fe4092a1ac77edd681aac13379b4
David Kegley <david@kegley.me>
2026-06-02 22:19:12 -0400

    Refactor Zed module to manage config files only

--- modules/zed.nix
+++ modules/zed.nix
@@ -1,15 +1,22 @@
-{ lib, config, pkgs, ... }:
-let cfg = config.kegs;
-in {
-  config = {
-    home.file = {
-      ".config/zed/keymap.json".source = ../config/zed/keymap.jsonc;
-      ".config/zed/settings.json".source = ../config/zed/settings.jsonc;
-    };
-    programs.zed-editor = {
-      enable = true;
-      # use system zed
-      package = null;
-    };
+{ config, pkgs, ... }: {
+  home.file = {
+    ".config/zed/keymap.json".source = ../config/zed/keymap.jsonc;
+    ".config/zed/settings.json".source = ../config/zed/settings.jsonc;
   };
+  # programs.zed-editor = {
+  #   enable = true;
+
+  #   # use system zed
+  #   # TODO: Why doesn't this allow null?
+  #   # package = null;
+
+  #   # TODO: Figure out mutable settings
+  #   # https://github.com/nix-community/home-manager/issues/6835
+  #   #
+  #   # reference an out-of-nix symlink so we can use the visual editor for the settings/keymap?
+  #   # xdg.configFile."zed/settings.json".source =
+  #   #   config.lib.file.mkOutOfStoreSymlink /path/to/your/dotfiles/zed/settings.json;
+  #   # or
+  #   # mutableUserSettings = true; ?
+  # };
 }
