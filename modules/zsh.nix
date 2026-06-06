e18b346998ba6a3e9dffd06c738482e66933eaa1
David Kegley <david@kegley.me>
2026-06-06 10:20:20 -0400

    Enhance Zsh history and add syntax highlighting

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -1,8 +1,10 @@
 { config, pkgs, ... }: {
   programs.zsh = {
     enable = true;
-    autosuggestion.enable = true;
     enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    
     sessionVariables = { EDITOR = "hx"; };
     shellAliases = {
       hm-update = "home-manager switch --flake $HOME/nix/#desktop";
@@ -10,5 +12,34 @@
       k = "kubectl";
       ll = "ls -al --color=auto";
     };
+
+    history = {
+      size = 100000;
+      save = 100000;
+      path = "$HOME/.zsh_history";
+      extended = true;
+      ignoreDups = true;
+      share = true;
+    };
+
+    initContent = ''
+      # Additional history options
+      setopt APPEND_HISTORY
+      setopt INC_APPEND_HISTORY
+      setopt HIST_SAVE_NO_DUPS
+      
+      # zsh up/down arrow history prefix search
+      autoload -Uz history-search-end
+      zle -N history-beginning-search-backward-end history-search-end
+      zle -N history-beginning-search-forward-end history-search-end
+      
+      # Bind arrow keys - using escape sequences directly as fallback
+      bindkey "^[[A" history-beginning-search-backward-end  # Up arrow
+      bindkey "^[[B" history-beginning-search-forward-end   # Down arrow
+      
+      # Also bind using terminfo if available
+      [[ -n "$terminfo[kcuu1]" ]] && bindkey "$terminfo[kcuu1]" history-beginning-search-backward-end
+      [[ -n "$terminfo[kcud1]" ]] && bindkey "$terminfo[kcud1]" history-beginning-search-forward-end
+    '';
   };
 }
