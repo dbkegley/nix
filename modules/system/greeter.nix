53cdc6dd5130d5eabd6311738842aed3263e32a9
David Kegley <david@kegley.me>
2026-07-01 17:49:46 -0400

    Theme tuigreet and replace greetd config file

--- modules/system/greeter.nix
+++ modules/system/greeter.nix
@@ -1,5 +1,8 @@
 { ... }:
 let
+  # https://github.com/apognu/tuigreet#theming
+  theme = "border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red";
+
   # tuigreet is provided by the `greetd-tuigreet` pacman package (binary: tuigreet).
   #   --time             show a clock
   #   --remember         remember the last logged-in username
@@ -7,20 +10,25 @@ let
   #   --asterisks        mask the password with asterisks
   #   --sessions         directory of Wayland session desktop entries to offer;
   #                      tuigreet lets you switch between them with F3.
+  #   --theme            component=color;... styling (see `theme` above).
   greeter =
     "tuigreet --time --remember --remember-session --asterisks "
-    + "--sessions /usr/share/wayland-sessions --cmd niri-session";
+    + "--sessions /usr/share/wayland-sessions --cmd niri-session "
+    + "--theme '${theme}'";
 in
 {
   config = {
-    environment.etc."greetd/config.toml".text = ''
-      [terminal]
-      vt = 1
+    environment.etc."greetd/config.toml" = {
+      replaceExisting = true;
+      text = ''
+        [terminal]
+        vt = 1
 
-      [default_session]
-      command = "${greeter}"
-      user = "greeter"
-    '';
+        [default_session]
+        command = "${greeter}"
+        user = "greeter"
+      '';
+    };
 
     systemd.services.greetd = {
       enable = true;
